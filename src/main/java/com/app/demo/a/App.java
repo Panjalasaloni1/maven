27061ca73f3e8b16a6af8f2fdc2e08cf72061ddb
saloni-077 <saloni812005@gmail.com>
2025-01-19 18:43:55 +0530

    Maven

---
+++ src/main/java/com/app/demo/a/App.java
@@ -0,0 +1,13 @@
+package com.app.demo.a;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
